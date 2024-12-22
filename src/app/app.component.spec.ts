1a85b6540d3da7358abf9be3c4e44584b117121d
Reza Madahi <reza.maddahi@outlook.com>
2024-12-23 03:12:38 +0330

    initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'CV' title`, () => {
+  it(`should have the 'cv' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CV');
+    expect(app.title).toEqual('cv');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CV');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cv');
   });
 });
